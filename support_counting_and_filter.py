fe08f1a5f8a5e2e9b5cbda227d6c422fc62a1226
Mohamed Yassin <myassinelsafty@gmail.com>
2025-11-09 20:00:44 +0200

    Split count_support function, added count_support_for_itemset function

--- support_counting_and_filter.py
+++ support_counting_and_filter.py
@@ -1,13 +1,16 @@
-# returns dict {candidate(frozenset): support(int)}
 def count_support(candidate_itemset_list, transactions_list):
     candidate_support_dict = {candidate: 0 for candidate in candidate_itemset_list}
-    for transaction in transactions_list:
-        for candidate in candidate_itemset_list:
-            if candidate.issubset(transaction):
-                candidate_support_dict[candidate] += 1
+    for candidate in candidate_itemset_list:
+        candidate_support_dict[candidate] = count_support_for_itemset(candidate, transactions_list)
     return candidate_support_dict
 
-# returns dict {candidate(frozenset): support(int)} where support > min_support
+def count_support_for_itemset(candidate_itemset, transactions_list):
+    support = 0
+    for transaction in transactions_list:
+        if candidate_itemset.issubset(transaction):
+            support += 1
+    return support
+
 def filter_frequent(candidate_itemset_support_dict, min_support):
     filtered_candidate_itemset_support_dict = {}
     for candidate, support in candidate_itemset_support_dict.items():
