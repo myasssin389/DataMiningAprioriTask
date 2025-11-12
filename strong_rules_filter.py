83804092ba6ac477d7597974aaef1265a64e0e91
mtahssen <mtahssen29@gmail.com>
2025-11-12 20:29:22 +0200

    Update strong_rules_filter.py

--- strong_rules_filter.py
+++ strong_rules_filter.py
@@ -5,3 +5,10 @@ filter_strong_rules(...)
 input: list of rules with calculated confidence, min_confidence
 output: list of strong rules
 """
+import pandas as pd
+
+strongrules = rules[(rules['confidence'] >= 0.6) & (rules['support'] >= min_support_val)].copy()
+strongrules = strongrules.sort_values(['lift', 'confidence', 'support'], ascending=False)
+
+col = ['support', 'confidence', 'lift']
+print(strongrules[cols_to_show].head(15))
