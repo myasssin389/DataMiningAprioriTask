99ea08783a2b6e35a7b6e3d4d5987b8eb961daf9
محمد صابر توفيق حنفي <2023170503@cis.asu.edu.eg>
2025-11-12 21:58:18 +0200

    Implement rule generation using dataclass
    Refactor rule generation to use Rule dataclass.

--- rule_generation.py
+++ rule_generation.py
@@ -1,10 +1,29 @@
-"""
-Function Requirements:
+from dataclasses import dataclass
+from typing import FrozenSet, Dict, List
+from itertools import combinations
 
-generate_rules(...)
-input: dict of frequent itemsets (frozenset): support
-output: rules list (all possible rules)
-note: support_A_U_B and support_B should be included
+@dataclass
+class Rule:
+    A: FrozenSet[str]
+    B: FrozenSet[str]
+    support_A_U_B: int
+    support_A: int
+    support_B: int
 
-see: rule_dataclass for rule declaration
-"""
+def generate_rules(frequent_itemsets: Dict[FrozenSet[str], int]) -> List[Rule]:
+    rules: List[Rule] = []
+
+    for itemset in frequent_itemsets:
+        if len(itemset) < 2:
+            continue
+
+        s = list(itemset)
+        for i in range(1, len(s)):
+            for combo in combinations(s, i):
+                A = frozenset(combo)
+                B = itemset - A
+                if B in frequent_itemsets:
+                    rules.append(Rule(A, B, frequent_itemsets[itemset], frequent_itemsets[A], frequent_itemsets[B]))
+
+    return rules
+
