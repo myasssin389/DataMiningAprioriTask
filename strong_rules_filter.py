3e4ef66a04134c88246bf93403fd0ea041f4cd32
mtahssen <mtahssen29@gmail.com>
2025-11-12 21:50:22 +0200

    Update strong_rules_filter.py

--- strong_rules_filter.py
+++ strong_rules_filter.py
@@ -6,9 +6,16 @@ input: list of rules with calculated confidence, min_confidence
 output: list of strong rules
 """
 import pandas as pd
+data=[{'1':{'d'},'2':{'m'},'c': 0.9},{'1': {'x'},'2': {'y'},'c': 0.3},{'1': {'t'},'2': {'n'},'c': 0.7}]          """  c -> conf.  """
+def srs(rules, mc):  """ mc -> min conf.  ,  srs -> strong rules"""
+ if isinstance(rules, pd.DataFrame):
+  sr=rules[rules['c'] >= min_confidence]   """  sr -> strong rule """
+  return sr.reset_index(drop=True)
+ elif isinstance(rules, list):
+  sr=[rule for rule in rules if rule.get('c', 0) >= mc]
+  return sr
+   
 
-strongrules = rules[(rules['confidence'] >= 0.6) & (rules['support'] >= min_support_val)].copy()
-strongrules = strongrules.sort_values(['lift', 'confidence', 'support'], ascending=False)
 
-col = ['support', 'confidence', 'lift']
-print(strongrules[cols_to_show].head(15))
+srf = srs(data, 0.5)      """   srf -> strong rules filtering """
+print(srf)
