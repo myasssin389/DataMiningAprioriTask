f0aad83435385184a47511d60c259d5b370d3020
Mohamedkhaled667 <mohahmedkhaled269@gmail.com>
2025-11-12 23:18:09 +0200

    Update confidence_computing.py

--- confidence_computing.py
+++ confidence_computing.py
@@ -1,8 +1,23 @@
-"""
-Function Requirements:
+def compute_confidence(rules):
+  
+    out = []
+    for r in rules:
+        supp_rule = r.get('support_rule', None)
+        supp_ant = r.get('support_antecedent', None)
 
+        if supp_rule is None or supp_ant is None:
+            # skip or set to None if missing inputs
+            new_r = dict(r)
+            new_r['confidence'] = None
+            out.append(new_r)
+            continue
 
-compute_confidence(...)
-input: list of rules
-output: list of rules with calculated confidence
-"""
+        if supp_ant == 0:
+            conf = 0.0
+        else:
+            conf = supp_rule / supp_ant
+
+        new_r = dict(r)
+        new_r['confidence'] = conf
+        out.append(new_r)
+    return out
