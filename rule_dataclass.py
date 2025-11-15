fd7722a97a959027e63f0d55cc0a1cb8873f3d87
Mohamed Yassin <myassinelsafty@gmail.com>
2025-11-15 09:56:59 +0200

    Added a function to print rules

--- rule_dataclass.py
+++ rule_dataclass.py
@@ -1,9 +1,6 @@
 from dataclasses import dataclass
 from typing import FrozenSet, Optional
-
-"""
-Rule = A -> B
-"""
+from frozenset_formatter import get_formatted_frozenset
 
 @dataclass
 class Rule:
@@ -14,3 +11,12 @@ class Rule:
     support_B: int
     confidence: Optional[float] = None
     lift: Optional[float] = None
+
+    def print_rule(self):
+        A = get_formatted_frozenset(self.A)
+        B = get_formatted_frozenset(self.B)
+        print("Rule: ", A, " -> ", B)
+        if self.confidence is not None:
+            print("Confidence = ", self.confidence)
+        if self.lift is not None:
+            print("Lift = ", self.lift)
